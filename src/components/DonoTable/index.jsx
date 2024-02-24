8e3708ea85e03696c919dbc06f8bad88cce2b275
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-24 11:35:03 -0500

    show menu dinamic in select

--- src/components/DonoTable/index.jsx
+++ src/components/DonoTable/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TableContainer,
   Table,
@@ -7,16 +7,38 @@ import {
   TableRow,
   TableCell,
   Container,
+  Select,
+  MenuItem,
+  Box,
 } from "@mui/material";
 import { data } from "../../data";
 import "./DonoTable.css";
 
+// Obtenemos el primer objeto de la lista
+const primerObjeto = data[0];
+// Obtenemos las claves del primer objeto
+const titulos = Object.keys(primerObjeto);
+// console.log(titulos);
+const titulosFiltro = Object.keys(data[0]).filter(
+  (key, index) => index !== 0 && index !== 4
+);
+const valoresUnicosPorTitulo = titulosFiltro.map(titulo => {
+    const valoresUnicosSet = new Set();
+    data.forEach(objeto => {
+      valoresUnicosSet.add(objeto[titulo]);
+    });
+    return Array.from(valoresUnicosSet);
+  });
+
+//   console.log(valoresUnicosPorTitulo);
+
+
 const DonoTable = () => {
-  // Obtenemos el primer objeto de la lista
-  const primerObjeto = data[0];
-  // Obtenemos las claves del primer objeto
-  const titulos = Object.keys(primerObjeto);
-  // console.log(titulos);
+  const [selectValue, setSelectValue] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedDependency(event.target.value);
+  };
 
   return (
     <>
@@ -24,15 +46,64 @@ const DonoTable = () => {
       <Container
         sx={{
           bgcolor: "white",
-          boxShadow:3,
+          boxShadow: 3,
         }}
       >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent:"space-between",
+            color: "black",
+            fontSize: "1.4rem",
+            p: "12px",
+          }}
+        >
+            {titulosFiltro.map((key, index) => (
+                <Box key={index}
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap:"5px",
+                    p: "12px",
+                  }}
+                >
+                    <p>{key}</p>
+                    <Select>
+                        <MenuItem>Todo el contenido</MenuItem>
+                        {valoresUnicosPorTitulo[index].map((valor, idx) => (
+                            <MenuItem key={idx}>{valor}</MenuItem>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Box>
+        {/* <div className="containSelects">
+        <label>{title}</label>    
+        <Select value={select} onChange={handle}>
+        <MenuItem value="">All Dependencies</MenuItem>
+        {menu.map((item, index) => (
+            <MenuItem key={index} value={item}>
+            {item}
+            </MenuItem>
+        ))}
+        </Select>
+
+        </div> */}
         <TableContainer>
-          <Table >
+          <Table>
             <TableHead>
-              <TableRow >
+              <TableRow>
                 {titulos.map((key, index) => (
-                  <TableCell key={index} sx={{ fontSize: "1.2rem" }}>{key}</TableCell>
+                  <TableCell
+                    key={index}
+                    sx={{
+                      fontSize: "1.4rem",
+                      color: "red",
+                    }}
+                  >
+                    {key}
+                  </TableCell>
                 ))}
               </TableRow>
             </TableHead>
@@ -40,7 +111,9 @@ const DonoTable = () => {
               {data.map((fila, index) => (
                 <TableRow key={index}>
                   {titulos.map((titulo, index) => (
-                    <TableCell key={index} sx={{ fontSize: "1.2rem" }}>{fila[titulo]}</TableCell>
+                    <TableCell key={index} sx={{ fontSize: "1.2rem" }}>
+                      {fila[titulo]}
+                    </TableCell>
                   ))}
                 </TableRow>
               ))}
